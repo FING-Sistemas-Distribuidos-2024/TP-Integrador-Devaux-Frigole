f72bd7ab8f50077ab605e7f94b5986223495a156
Francisco Devaux <frandevaux2013@gmail.com>
2024-06-12 01:43:24 -0300

    Better ui 2.0

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,24 +3,24 @@
 import React, { useState, useCallback, useEffect } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
 
-const WebSocketDemo = () => {
+export const WebSocketDemo = () => {
   // Public API that will echo messages sent to it back to the client
-  const [socketUrl, setSocketUrl] = useState("ws://localhost:4000");
-  const [messageHistory, setMessageHistory] = useState<String[]>([]);
+  const [socketUrl, setSocketUrl] = useState("ws://127.0.0.1:4000");
+  const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>([]);
   const [message, setMessage] = useState("");
 
   const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
 
   useEffect(() => {
     if (lastMessage !== null) {
-      const reader = new FileReader();
-      reader.readAsText(lastMessage.data);
-      console.log("Message received: ", lastMessage.data);
+      console.log("Message received: ", lastMessage);
+      setMessageHistory((prev) => prev.concat(lastMessage));
     }
   }, [lastMessage]);
 
   const handleClickSendMessage = useCallback(() => {
     sendMessage(message);
+    setMessage("");
     console.log("Message sent: ", message);
   }, [message, sendMessage]);
 
@@ -28,7 +28,7 @@ const WebSocketDemo = () => {
     <div>
       <div className="w-screen h-screen flex flex-col items-center gap-3">
         <h1 className="flex pt-5">Chat App</h1>
-        <div className="w-2/3 flex flex-col bg-neutral-800 p-5 gap-2 h-5/6 rounded-lg   ">
+        <div className="w-2/3 flex flex-col bg-neutral-800 p-5 gap-2 h-5/6 rounded-lg max-h-full  overflow-y-auto ">
           <div className="flex justify-center ">Messages</div>
           {messageHistory.map((msg, index) => (
             <div className="bg-neutral-700 py-1 px-2 rounded-lg" key={index}>
@@ -42,6 +42,11 @@ const WebSocketDemo = () => {
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             placeholder="Type a message"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                handleClickSendMessage();
+              }
+            }}
           />
           <div className="flex justify-between w-1/2 gap-2 ">
             <button
